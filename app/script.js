722d4ff9f34a82da8a488c4d2167ad0024f32d08
Simon A. Eugster <simon.eu@gmail.com>
2017-12-04 22:57:28 +0100

    Generate content from data and use very beautiful CSSb

--- app/script.js
+++ app/script.js
@@ -1,27 +1,29 @@
-function makeField( language, countryName ) {
+function makeBox( boxName, boxValue ) {
+
     const div = document.createElement( 'div' );
-    const lang = document.createElement( 'span' );
     const name = document.createElement( 'span' );
+    const value = document.createElement( 'span' );
 
-    lang.textContent = language;
-    name.textContent = name;
+    div.classList.add( 'box' );
+    name.classList.add( 'name' );
+    value.classList.add( 'value' );
 
-    div.appendChild( lang );
-    div.appendChild( countryName );
+    name.textContent = boxName;
+    value.textContent = boxValue;
+
+    div.appendChild( name );
+    div.appendChild( value );
 
     return div;
 }
 
-fetch( 'https://www.wikidata.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=Q39' ).then( r => {
-    r.json().then( data => {
-        let realData = data.query.pages[ '153' ].revisions[ 0 ][ '*' ];
-        let ch = JSON.parse( realData );
-        console.log( ch.labels );
-        for ( let entry in ch.labels ) {
-            let name = ch.labels[ entry ].value;
-            let field = makeField( entry, name );
-            document.body.appendChild( field );
-            console.log( `${entry}: ${name}` )
-        }
-    } )
+const data = [
+    { name: 'Banana', value: 42 },
+    { name: 'Orange', value: 33 },
+    { name: 'Boskoop', value: 50 },
+];
+
+data.forEach( entry => {
+    const box = makeBox( entry.name, entry.value );
+    document.body.appendChild( box );
 } );
